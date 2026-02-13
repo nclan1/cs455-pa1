d848da46338d14ab6fe5f489d37661005e9c6c93
nolan <144292887+nclan1@users.noreply.github.com>
2026-02-13 10:14:56 -0500

    TCPClient add ip

--- TCPServer.py
+++ TCPServer.py
@@ -1,13 +1,15 @@
 from socket import *
+
 serverPort = 12000
-serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket = socket(AF_INET, SOCK_STREAM)
 # This is the "Welcoming door"
-serverSocket.bind(('',serverPort))
+# "" means listening on all available network interface
+serverSocket.bind(("", serverPort))
 serverSocket.listen(1)
-print('The server is ready to receive')
+print("The server is ready to receive")
 while True:
     connectionSocket, addr = serverSocket.accept()
-    #This is the dedicated socket
+    # This is the dedicated socket
     sentence = connectionSocket.recv(1024).decode()
     capitalizedSentence = sentence.upper()
     connectionSocket.send(capitalizedSentence.encode())
