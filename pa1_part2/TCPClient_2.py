803024797e124aa35ba973745ea537a319c4137b
nclan1 <nclan@bu.edu>
2026-02-23 11:38:23 -0500

    started argparse implementation for server

--- pa1_part2/TCPClient_2.py
+++ pa1_part2/TCPClient_2.py
@@ -1,22 +1,55 @@
+import argparse
 from socket import *
 
-serverName = input("Input host name or IP: ")
-# serverName = "127.0.0.1"
-serverPort = int(input("Input port number: "))
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
+# set up parser
+parser = argparse.ArgumentParser(
+    description="Python Client acting as a server to perform RTT and Throughput Measurements"
+)
 
-# TODO: refactor to use argparse? could be useful
+parser.add_argument(
+    "--host",
+    help="The hostname / IP address to connect to. Default: localhost",
+    default="localhost",
+)
+parser.add_argument(
+    "-p",
+    "--port",
+    type=int,
+    help="The port number to connect to. Default: 9090",
+    default=9090,
+)
+
+parser.add_argument(
+    "-ms",
+    "--measurement-type",
+    help="Specify whether to compute the RTT (Round Trip Time) or throughput.",
+    choices=["rtt", "tput"],
+    required=True,
+)
+
+parser.add_argument(
+    "-n",
+    "--probes",
+    help="Number of measurement probes that the server should expect to receive",
+)
+
+# parser.add_argument("-ms", "--measurement-type")
+
+# clientSocket = socket(AF_INET, SOCK_STREAM)
+# clientSocket.connect((serverName, serverPort))
+
 
 # after setting up the connection, send a message with the following format
 # <PROTOCOL PHASE><WS><MEASUREMENT TYPE><WS><NUMBER OF PROBES><WS><MESSAGE SIZE><WS><SERVER DELAY>\n
 #
-measurement_type = input("Input 'rtt' or 'tput': ")
-num_probes = input("Input number of probes: ")
-msg_size = int(input("Input bytes in payload: "))
-server_delay = int(input("Input amount of time server shoudl wait: (default 0)"), 0)
+# measurement_type = input("Input 'rtt' or 'tput': ")
+# num_probes = input("Input number of probes: ")
+# msg_size = int(input("Input bytes in payload: "))
+# server_delay = int(input("Input amount of time server shoudl wait: (default 0)"), 0)
 
 # clientSocket.send(TODO HERE)
-modifiedSentence = clientSocket.recv(1024)
-print("From Server:", modifiedSentence.decode())
-clientSocket.close()
+# modifiedSentence = clientSocket.recv(1024)
+# print("From Server:", modifiedSentence.decode())
+# clientSocket.close()
+#
+args = parser.parse_args()
